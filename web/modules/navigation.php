ffa8426744fc708a89a2333e8d73a8fbf6f00098
Ryan-Hafen <ryan.hafen@icloud.com>
2018-01-20 18:14:10 -0800

    bug fix

--- web/modules/navigation.php
+++ web/modules/navigation.php
@@ -6,14 +6,14 @@
  		<div class="col container l2">
 			<a href="/pages/assignments.php" class="bar-item hover-theme hover-text-theme hover-border-theme btn margin">Assignments</a>
 		</div>
-		// <div class="col container l2">
-			// <a href="/pages/portfolio.html" class="bar-item hover-theme hover-text-theme hover-border-theme btn margin">Portfolio</a>
-		// </div> 
-		// <div class="col container l2">
-			// <button class="bar-item hover-theme hover-text-theme hover-border-theme btn margin" onclick="document.getElementById('id02').style.display='block'">Contact</button>
-		// </div>
-		// <div class="col container l2">
-			// <button class="bar-item hover-theme hover-text-theme hover-border-theme btn margin" onclick="document.getElementById('id01').style.display='block'">About</button>
-		// </div>
+		 <!-- <div class="col container l2"> -->
+			 <!-- <a href="/pages/portfolio.html" class="bar-item hover-theme hover-text-theme hover-border-theme btn margin">Portfolio</a> -->
+		 <!-- </div>  -->
+		 <!-- <div class="col container l2"> -->
+			 <!-- <button class="bar-item hover-theme hover-text-theme hover-border-theme btn margin" onclick="document.getElementById('id02').style.display='block'">Contact</button> -->
+		 <!-- </div> -->
+		 <!-- <div class="col container l2"> -->
+			 <!-- <button class="bar-item hover-theme hover-text-theme hover-border-theme btn margin" onclick="document.getElementById('id01').style.display='block'">About</button> -->
+		 <!-- </div> -->
 	</div>
 </div>
