33e108eb275d384dd416141969fbb2a12ba0b3b2
Ryan-Hafen <ryan.hafen@icloud.com>
2018-01-20 18:35:08 -0800

    fixes

--- web/modules/footer.php
+++ web/modules/footer.php
@@ -1,8 +1,8 @@
 <footer class="container center theme-light margin-top padding">
     <a href="#myHeader" class="bar-item btn"></i>Home</a>
-/*     <a href="practice/index.html" class="bar-item btn"></i>Practice</a>
-    <a href="styleguide/styleguide.html" class="bar-item btn"></i>Style Guide</a> */
+    <!-- <a href="practice/index.html" class="bar-item btn"></i>Practice</a> -->
+    <!-- <a href="styleguide/styleguide.html" class="bar-item btn"></i>Style Guide</a> -->
 	<button class="bar-item btn" onclick="document.getElementById('id01').style.display='block'">About</button>
 </footer>
   
-/* <script src="/javascript/analytics.js"></script> */
+ <!-- <script src="/javascript/analytics.js"></script> -->
