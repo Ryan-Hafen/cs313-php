d2e3751a7c56c9d60bca39c4c1996983893a6fab
Ryan-Hafen <ryan.hafen@icloud.com>
2018-01-20 18:42:09 -0800

    fixed logo in header

--- web/modules/header.php
+++ web/modules/header.php
@@ -1,6 +1,6 @@
  <header class="container theme-light padding" id="myHeader">
 	<div class="display-topleft padding">
-		<img class="animate-opacity animate-left" src="images/logo.png" style="margin:auto;">
+		<img class="animate-opacity animate-left" src="../images/logo.png" style="margin:auto;">
 	</div>
 	<div class="center">
 		<h1 class="xxxlarge">Ryan Hafen</h1>
