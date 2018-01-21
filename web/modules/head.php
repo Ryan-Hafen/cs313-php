df7de362e74722c9ca67e3f74367965cec7b27e8
Ryan-Hafen <ryan.hafen@icloud.com>
2018-01-20 18:06:47 -0800

    fixing css location error

--- web/modules/head.php
+++ web/modules/head.php
@@ -3,5 +3,5 @@
 <meta name="description" content="CS 313 personal homepage">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" type="image/png" sizes="16x16" href="images/logo.png">
-<link href="css/style.css" rel="stylesheet" type="text/css" media="screen">
+<link href="../css/style.css" rel="stylesheet" type="text/css" media="screen">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
