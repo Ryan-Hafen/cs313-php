50299c41b806a078b3a71365c42ecf921db9a707
RyanHafen <ryan.hafen@hotmail.com>
2018-02-13 19:23:06 -0800

    bug fix

--- web/pages/project/page_journal/notes_list.php
+++ web/pages/project/page_journal/notes_list.php
@@ -3,7 +3,7 @@ require $_SERVER['DOCUMENT_ROOT'].'/pages/project/model/database.php';
 include $_SERVER['DOCUMENT_ROOT'].'/pages/project/view/header.php'; 
 
 // Get all categories
-$query = 'SELECT * 
+$query = 'SELECT bookname
             FROM books
            ORDER BY ID';
 $statement = $db->prepare($query);
@@ -20,18 +20,11 @@ $statement->closeCursor();
     <table>
         <tr>
             <th>Name</th>
-            <th>&nbsp;</th>
         </tr>
         
         <?php foreach ($books as $book) : ?>
         <tr>    
-            <td><?php echo $book['bookName']; ?></td>
-            <td><form action="delete_category.php" method="post">
-                    <input type="hidden" name="book_ID"
-                        value="<?php echo $book['ID']; ?>">
-                    <input type="submit" value="Delete">
-                </form>
-            </td>
+            <td><?php echo $book['bookname']; ?></td>
         </tr>
         <?php endforeach; ?>
     </table>
