13a77fb5348bf58d281a78e314f038f62c657766
RyanHafen <ryan.hafen@hotmail.com>
2018-02-18 22:41:44 -0800

    i suck

--- web/pages/project/notes_list.php
+++ web/pages/project/notes_list.php
@@ -1,5 +1,5 @@
 <?php 
-// require $_SERVER['DOCUMENT_ROOT'].'/pages/project/model/database.php';
+require 'database.php';
 include 'header.php'; 
 
 $current_page = htmlspecialchars($_SERVER["PHP_SELF"]);
