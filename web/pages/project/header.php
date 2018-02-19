7d9a5ed7239bab5b8fdf290f08e07eb5cc2520c6
RyanHafen <ryan.hafen@hotmail.com>
2018-02-18 22:27:43 -0800

    i suck

---
+++ web/pages/project/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Scripture Journal</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header><h1>My Scripture Journal</h1></header>
--- web/pages/project/index.php
+++ web/pages/project/index.php
@@ -1,5 +1,5 @@
 <?php
-require $_SERVER['DOCUMENT_ROOT'].'/pages/project/model/database.php';
+require 'database.php';
 // require $_SERVER['DOCUMENT_ROOT'].'/pages/project/model/notes_db.php');
 // require $_SERVER['DOCUMENT_ROOT'].'/pages/project/model/scriptures_db.php');
 // require $_SERVER['DOCUMENT_ROOT'].'/pages/project/model/users_db.php');
