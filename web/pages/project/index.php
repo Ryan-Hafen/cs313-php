74c9fed1eb9ab6e03ede3bc971fbecebef545105
RyanHafen <ryan.hafen@hotmail.com>
2018-02-18 22:30:16 -0800

    i suck

--- web/pages/project/index.php
+++ web/pages/project/index.php
@@ -5,13 +5,13 @@ require 'scriptures_db.php';
 require 'users_db.php';
 
 print 'index.php'
-// $action = filter_input(INPUT_POST, 'action');
-// if ($action == NULL) {
-    // $action = filter_input(INPUT_GET, 'action');
-    // if ($action == NULL) {
-        // $action = 'list_notes';
-    // }
-// }  
+$action = filter_input(INPUT_POST, 'action');
+if ($action == NULL) {
+    $action = filter_input(INPUT_GET, 'action');
+    if ($action == NULL) {
+        $action = 'list_notes';
+    }
+}  
 
 // if ($action == 'list_notes') {
     // $notes = get_notes();
