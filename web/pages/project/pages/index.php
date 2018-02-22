9c3ad22f726e2628099b23fe97421fe46271386d
RyanHafen <ryan.hafen@hotmail.com>
2018-02-21 22:47:46 -0800

    sign in update

--- web/pages/project/pages/index.php
+++ web/pages/project/pages/index.php
@@ -39,9 +39,8 @@ else if ($action == 'sign_in'){
 	} 
 	else { 
 		$_SESSION["user_id"] = get_user_by_email($email);
-		echo $_SESSION["user_id"];
-		// $notes = get_notes($$_SESSION["user_id"]);
-		// include('notes_list.php');
+		$notes = get_notes($_SESSION["user_id"]);
+		include('notes_list.php');
 	} 
 
  } 
