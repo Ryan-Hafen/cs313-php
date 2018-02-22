b5849ed6e93f4529c29277bc54b3fae463cd4f79
RyanHafen <ryan.hafen@hotmail.com>
2018-02-21 21:28:13 -0800

    sign in update

--- web/pages/project/model/users_db.php
+++ web/pages/project/model/users_db.php
@@ -1,29 +1,29 @@
 <?php
 
-function get_user_data($user_id) {
-    global $db;
-    $query = 'SELECT * 
-                FROM users
-               WHERE id = :user_id';    
-    $statement = $db->prepare($query);
-    $statement->bindValue(':user_id', $user_id);
-    $statement->execute();
-    return $statement;    
-}
+// function get_user_data($user_id) {
+    // global $db;
+    // $query = 'SELECT * 
+                // FROM users
+               // WHERE id = :user_id';    
+    // $statement = $db->prepare($query);
+    // $statement->bindValue(':user_id', $user_id);
+    // $statement->execute();
+    // return $statement;    
+// }
 
-function get_user_by_email($email) {
-    global $db;
-    $query = 'SELECT id 
-                FROM users
-               WHERE email = :email';    
-    $statement = $db->prepare($query);
-    $statement->bindValue(':email', $email);
-    $statement->execute();
-    $user = $statement->fetch();
-    $statement->closeCursor();
-    $user_id = $user['id'];
-    return $user_id; 
-}
+// function get_user_by_email($email) {
+    // global $db;
+    // $query = 'SELECT id 
+                // FROM users
+               // WHERE email = :email';    
+    // $statement = $db->prepare($query);
+    // $statement->bindValue(':email', $email);
+    // $statement->execute();
+    // $user = $statement->fetch();
+    // $statement->closeCursor();
+    // $user_id = $user['id'];
+    // return $user_id; 
+// }
 
 // function get_password($email) {
     // global $db;
