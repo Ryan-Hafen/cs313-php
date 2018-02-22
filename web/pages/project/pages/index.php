3122d66aa559b2f2dace6662cd5472c903f35423
RyanHafen <ryan.hafen@hotmail.com>
2018-02-21 20:19:14 -0800

    add sign in function

--- web/pages/project/pages/index.php
+++ web/pages/project/pages/index.php
@@ -15,97 +15,98 @@ if ($action == NULL) {
 if ($action == 'sign_in'){
     include('sign_in.php');
 
-} else if ($action == 'register_user') {
-    include('register_user.php');
+} 
+// else if ($action == 'register_user') {
+    // include('register_user.php');
 
-} else if ($action == 'list_notes') {
-    $email = filter_input(INPUT_POST, 'email');
-	$user_id = get_user_by_email($email);
+// } else if ($action == 'list_notes') {
+    // $email = filter_input(INPUT_POST, 'email');
+	// $user_id = get_user_by_email($email);
 	
-	if($note_id != false) {
+	// if($note_id != false) {
 		
-		$notes = get_notes();
-		include('notes_list.php');
-	}
-}	
-} else if ($action == 'edit_note_form') {
-    $note_id = filter_input(INPUT_POST, 'note_id', FILTER_VALIDATE_INT);
-    $book_id = filter_input(INPUT_POST, 'book_id', FILTER_VALIDATE_INT);
-    $volume_id = filter_input(INPUT_POST, 'volume_id', FILTER_VALIDATE_INT);
-    $chapter_id = filter_input(INPUT_POST, 'chapter', FILTER_VALIDATE_INT);
-    $verse_id = filter_input(INPUT_POST, 'verse', FILTER_VALIDATE_INT);
+		// $notes = get_notes();
+		// include('notes_list.php');
+	// }
+// }	
+// } else if ($action == 'edit_note_form') {
+    // $note_id = filter_input(INPUT_POST, 'note_id', FILTER_VALIDATE_INT);
+    // $book_id = filter_input(INPUT_POST, 'book_id', FILTER_VALIDATE_INT);
+    // $volume_id = filter_input(INPUT_POST, 'volume_id', FILTER_VALIDATE_INT);
+    // $chapter_id = filter_input(INPUT_POST, 'chapter', FILTER_VALIDATE_INT);
+    // $verse_id = filter_input(INPUT_POST, 'verse', FILTER_VALIDATE_INT);
     
-    if ($note_id != false) {
-        $note = get_note($note_id);
-        $volumes = get_volume_list();
-        $books = get_book_list();
-        $chapters = get_chapter_list();
-        $verses = get_verse_list();
+    // if ($note_id != false) {
+        // $note = get_note($note_id);
+        // $volumes = get_volume_list();
+        // $books = get_book_list();
+        // $chapters = get_chapter_list();
+        // $verses = get_verse_list();
         
-        include('edit_note_form.php');
-    }
-} else if ($action == 'edit_note') {
+        // include('edit_note_form.php');
+    // }
+// } else if ($action == 'edit_note') {
 	
-    $note_id = filter_input(INPUT_POST, 'note_id', FILTER_VALIDATE_INT);
-    $book_id = filter_input(INPUT_POST, 'book_id', FILTER_VALIDATE_INT);
-    $volume_id = filter_input(INPUT_POST, 'volume_id', FILTER_VALIDATE_INT);
-    $chapter_id = filter_input(INPUT_POST, 'chapter_id', FILTER_VALIDATE_INT);
-    $verse_id = filter_input(INPUT_POST, 'verse_id', FILTER_VALIDATE_INT);
-    $note_text = filter_input(INPUT_POST, 'note_text');
+    // $note_id = filter_input(INPUT_POST, 'note_id', FILTER_VALIDATE_INT);
+    // $book_id = filter_input(INPUT_POST, 'book_id', FILTER_VALIDATE_INT);
+    // $volume_id = filter_input(INPUT_POST, 'volume_id', FILTER_VALIDATE_INT);
+    // $chapter_id = filter_input(INPUT_POST, 'chapter_id', FILTER_VALIDATE_INT);
+    // $verse_id = filter_input(INPUT_POST, 'verse_id', FILTER_VALIDATE_INT);
+    // $note_text = filter_input(INPUT_POST, 'note_text');
 	
 	
     
-    if ($note_id == false || $book_id == false || $chapter_id == false || $verse_id == false || $note_text == "") {
-        $error = 'All fields are required. note_id = ' .$note_id;
-        include('../errors/error.php');
-    } else {
-		$scriptures_id = get_scripture_id($book_id, $chapter_id, $verse_id);
+    // if ($note_id == false || $book_id == false || $chapter_id == false || $verse_id == false || $note_text == "") {
+        // $error = 'All fields are required. note_id = ' .$note_id;
+        // include('../errors/error.php');
+    // } else {
+		// $scriptures_id = get_scripture_id($book_id, $chapter_id, $verse_id);
 		
-        edit_note($note_id, $scriptures_id, $note_text);
-        header("Location: .?note_id=$note_id");
-        include('notes_list.php');
-    }
-} else if ($action == 'delete_note') {
-    $note_id = filter_input(INPUT_POST, 'note_id', FILTER_VALIDATE_INT);
-    if ($note_id == false) {
-        $error = "Missing or incorrect product id or category id.";
-        include('../errors/error.php');
-    } else { 
-        delete_note($note_id);
-        header("Location: .?note_id=$note_id");
-        include('notes_list.php');
-    }
-} else if ($action == 'add_note_form') {
-    $volumes = get_volume_list();
-    $books = get_book_list();
-    $chapters = get_chapter_list();
-    $verses = get_verse_list();
-    include('add_note_form.php');
-} else if ($action == 'add_note') {
-    $note_text = filter_input(INPUT_POST, 'note_text');
-    $book_id = filter_input(INPUT_POST, 'book_id', FILTER_VALIDATE_INT);
-    $chapter_id = filter_input(INPUT_POST, 'chapter_id', FILTER_VALIDATE_INT);
-    $verse_id = filter_input(INPUT_POST, 'verse_id', FILTER_VALIDATE_INT);
+        // edit_note($note_id, $scriptures_id, $note_text);
+        // header("Location: .?note_id=$note_id");
+        // include('notes_list.php');
+    // }
+// } else if ($action == 'delete_note') {
+    // $note_id = filter_input(INPUT_POST, 'note_id', FILTER_VALIDATE_INT);
+    // if ($note_id == false) {
+        // $error = "Missing or incorrect product id or category id.";
+        // include('../errors/error.php');
+    // } else { 
+        // delete_note($note_id);
+        // header("Location: .?note_id=$note_id");
+        // include('notes_list.php');
+    // }
+// } else if ($action == 'add_note_form') {
+    // $volumes = get_volume_list();
+    // $books = get_book_list();
+    // $chapters = get_chapter_list();
+    // $verses = get_verse_list();
+    // include('add_note_form.php');
+// } else if ($action == 'add_note') {
+    // $note_text = filter_input(INPUT_POST, 'note_text');
+    // $book_id = filter_input(INPUT_POST, 'book_id', FILTER_VALIDATE_INT);
+    // $chapter_id = filter_input(INPUT_POST, 'chapter_id', FILTER_VALIDATE_INT);
+    // $verse_id = filter_input(INPUT_POST, 'verse_id', FILTER_VALIDATE_INT);
 	
 	
 		
-    if ($book_id == false) {
-        $error = "Invalid product data. Check all fields and try again.";
-        include('../errors/error.php');
-    } else {
-		$scriptures_id = get_scripture_id($book_id, $chapter_id, $verse_id);
+    // if ($book_id == false) {
+        // $error = "Invalid product data. Check all fields and try again.";
+        // include('../errors/error.php');
+    // } else {
+		// $scriptures_id = get_scripture_id($book_id, $chapter_id, $verse_id);
 		
-		// echo $book_id;
-		// echo $chapter_id;
-		// echo $verse_id;
-		// echo $scriptures_id;
-		// echo $note_text;
+		// // echo $book_id;
+		// // echo $chapter_id;
+		// // echo $verse_id;
+		// // echo $scriptures_id;
+		// // echo $note_text;
 		
-        add_note($note_text, $scriptures_id);
-        header("Location: .?note_id=$note_id");
-        include('notes_list.php');
-    }
-}
+        // add_note($note_text, $scriptures_id);
+        // header("Location: .?note_id=$note_id");
+        // include('notes_list.php');
+    // }
+// }
 ?>
 
 
