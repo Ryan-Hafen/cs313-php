4e407b200b5fe378f632251b686ede8ee288ee3a
RyanHafen <ryan.hafen@hotmail.com>
2018-02-22 19:10:30 -0800

    update footer

--- web/pages/project/view/header.php
+++ web/pages/project/view/header.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>My Scripture Journal</title>
-    <link rel="stylesheet" type="text/css" href="../web/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../view/style.css">
 </head>
 
 <!-- the body section -->
