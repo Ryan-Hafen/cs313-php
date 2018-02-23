10c2387581df392df7277d88d91d2f8a67e1c379
RyanHafen <ryan.hafen@hotmail.com>
2018-02-22 19:44:36 -0800

    update add notes

--- web/pages/project/pages/index.php
+++ web/pages/project/pages/index.php
@@ -142,10 +142,10 @@ else if ($action == 'add_note') {
     } else {
 		$scriptures_id = get_scripture_id($book_id, $chapter_id, $verse_id);
 		
-        // add_note($user_id, $scriptures_id, $note_text);
+        add_note($user_id, $scriptures_id, $note_text);
 		
-		// $notes = get_notes($user_id);
-		// include('notes_list.php');
+		$notes = get_notes($user_id);
+		include('notes_list.php');
     }
 }
 ?>
