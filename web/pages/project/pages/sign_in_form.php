2c7feeb5f79f625d02043a5a8265249898ae1ecf
RyanHafen <ryan.hafen@hotmail.com>
2018-02-24 02:44:11 -0800

    update ui

--- web/pages/project/pages/sign_in_form.php
+++ web/pages/project/pages/sign_in_form.php
@@ -1,22 +1,23 @@
 <?php include '../view/header.php'; ?>
-
-<div class="center">
-	<div class="half card-4 margin padding-16">
-		<h2 class="text-theme">Sign in</h2>
-    
-		<form action="." method="post" class="form">
-			<input type="hidden" name="action" value="sign_in">
-			<div class="section">
-				<input class="input" type="text" id="email" name="email" placeholder="Email" value="" /> 
-			</div>
-			<div class="section">
-				<input class="input" type="password" id="password" name="password" placeholder="Password" /> 
-			</div>
-			<div class="section">
-				<input class="button margin padding-16" type="submit" value="Sign In" /> 
-				<a href="index.php?action=register_form" class="button margin padding-16 theme">Register</a>
-			</div>
-		</form>
+<div class="container center">
+	<div class="row row-padding">
+		<div class="half card-4 margin padding-16">
+			<h2 class="text-theme">Sign in</h2>
+		
+			<form action="." method="post" class="form">
+				<input type="hidden" name="action" value="sign_in">
+				<div class="section">
+					<input class="input" type="text" id="email" name="email" placeholder="Email" value="" /> 
+				</div>
+				<div class="section">
+					<input class="input" type="password" id="password" name="password" placeholder="Password" /> 
+				</div>
+				<div class="section">
+					<input class="button margin padding-16" type="submit" value="Sign In" /> 
+					<a href="index.php?action=register_form" class="button margin padding-16 theme">Register</a>
+				</div>
+			</form>
+		</div>
 	</div>
 </div>
 
