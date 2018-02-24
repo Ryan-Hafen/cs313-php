d37eb68930c9903cf070359365f8f0de9ae22ca4
RyanHafen <ryan.hafen@hotmail.com>
2018-02-24 01:07:34 -0800

    ui imporvements

--- web/modules/head.php
+++ web/modules/head.php
@@ -4,4 +4,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="../images/logo.png" rel="icon" type="image/png" sizes="16x16">
 <link href="../css/style.css" rel="stylesheet" type="text/css" media="screen">
+<?php include $_SERVER['DOCUMENT_ROOT'].'/css/style.css';?>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.4/css/fontawesome.css">
