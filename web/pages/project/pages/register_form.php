6c10949171f177407e768b15dc51033d22b70156
RyanHafen <ryan.hafen@hotmail.com>
2018-02-23 21:38:43 -0800

    update registgration

--- web/pages/project/pages/register_form.php
+++ web/pages/project/pages/register_form.php
@@ -20,7 +20,7 @@
         <br>
     </form>
     <p class="last_paragraph">
-        <a href="index.php?action=sign_in">Sign In</a>
+        <a href="index.php?action=sign_in">Register</a>
     </p>
 
 </main>
