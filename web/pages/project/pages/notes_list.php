e7d84636fbcc698cbb31331310ea408c23cb15c6
RyanHafen <ryan.hafen@hotmail.com>
2018-02-24 02:14:35 -0800

    update ui

--- web/pages/project/pages/notes_list.php
+++ web/pages/project/pages/notes_list.php
@@ -6,7 +6,6 @@
 		<a href="index.php?action=add_note" class="left-align button circle large theme">
 			<i class="fas fa-plus"></i>
 		</a>
-		<p class="tooltip text">Add Note</p>
 	</div>
 		
 	<div class="center">
--- web/pages/project/pages/sign_in_form.php
+++ web/pages/project/pages/sign_in_form.php
@@ -1,21 +1,28 @@
 <?php include '../view/header.php'; ?>
+
 <main>
-    <h1>Sign in</h1>
+<div class="threequarter"
+	<div class="container card-4">
+		<h2>Sign in</h2>
     
-    <form action="." method="post" class="form">
-        <input type="hidden" name="action" value="sign_in">
-
-        <input type="text" id="email" name="email" placeholder="Email" value="" /> 
-        <br>
-        <input type="password" id="password" name="password" placeholder="Password" /> <span class="error<?php if (!$error) echo ' hide' ?>">*</span>
-        <br>           
-
-        <input type="submit" value="Sign In" />
-        <br>
-    </form>
-    <p class="last_paragraph">
-        <a href="index.php?action=register_form">Register</a>
-    </p>
+		<form action="." method="post" class="form">
+			<input type="hidden" name="action" value="sign_in">
+			<div class="section">
+				<input class="input" type="text" id="email" name="email" placeholder="Email" value="" /> 
+			</div>
+			<div class="section">
+				<input class="input" type="password" id="password" name="password" placeholder="Password" /> 
+			</div>
+			<div class="section">
+				<input class="button" type="submit" value="Sign In" /> 
+			</div>
+		</form>
+	</div>
+		
+	<div class="responsive padding-16">
+		<a href="index.php?action=register_form" class="button">Register</a>
+	</div>
 
 </main>
+
 <?php include '../view/footer.php'; ?>
