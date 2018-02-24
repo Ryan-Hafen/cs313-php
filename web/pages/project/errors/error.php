b431b7e9c5bd39cea1509165ef1d3eaa74f617f2
RyanHafen <ryan.hafen@hotmail.com>
2018-02-23 23:53:40 -0800

    updating registation

--- web/pages/project/errors/error.php
+++ web/pages/project/errors/error.php
@@ -1,6 +1,5 @@
-<?php include '../view/header.php'; ?>
+
 <div id="main">
     <h1 class="top">Error</h1>
     <p class="first_paragraph error"><?php echo $error; ?></p>
 </div>
-<?php include '../view/footer.php'; ?>
