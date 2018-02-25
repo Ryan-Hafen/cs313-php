cdb3530c2837dd46719e0220f15203e9b73b3d1e
RyanHafen <ryan.hafen@hotmail.com>
2018-02-24 17:30:12 -0800

    update

--- web/pages/project/view/footer.php
+++ web/pages/project/view/footer.php
@@ -1,5 +1,5 @@
 	
-	<footer class="container right theme-light margin-top padding">
+	<footer class="container right-align theme-light margin-top padding">
 		<p class="copyright">
 			&copy; <?php echo date("Y"); ?> Rucy Design, Inc.
 		</p>
